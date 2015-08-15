811a40695cb769497d96be568f347896dfcddc85
cody-ng <cody@Codys-MacBook-Pro.local>
2015-08-14 22:54:01 -0400

    Added Xaml page

---
+++ XamarinFormDemo/XamarinFormDemo/FirstXamlPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XamarinFormDemo
+{
+    public partial class FirstXamlPage : ContentPage
+    {
+        public FirstXamlPage ()
+        {
+            InitializeComponent ();
+        }
+
+        void OnColorSliderChanged(Object sender, EventArgs e)
+        {
+            var red = sliderRed.Value;
+            var green = sliderGreen.Value;
+            var blue = sliderBlue.Value;
+
+            boxviewColor.Color = Color.FromRgb (red, green, blue);
+        }
+    }
+}
+
