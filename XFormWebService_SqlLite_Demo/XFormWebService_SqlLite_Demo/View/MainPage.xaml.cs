b68866bc296c5419b779b1b1704f455c5c89b8a8
cody-ng <cody@Codys-MacBook-Pro.local>
2015-08-26 16:00:42 -0400

    Added Style and Trigger
    - explicit, implicit, inherited, and dynamic style
    - event trigger and style trigger

--- XFormWebService_SqlLite_Demo/XFormWebService_SqlLite_Demo/View/MainPage.xaml.cs
+++ XFormWebService_SqlLite_Demo/XFormWebService_SqlLite_Demo/View/MainPage.xaml.cs
@@ -8,6 +8,7 @@ namespace XFormWebService_SqlLite_Demo
     public partial class MainPage : ContentPage
     {
         MainPageVM _vm;
+        bool _done = false;
 
         public MainPage ()
         {
@@ -17,13 +18,41 @@ namespace XFormWebService_SqlLite_Demo
             InitializeComponent ();
         }
 
-        public async void OnClicked(object o, EventArgs e){
+        public async void OnClicked (object o, EventArgs e)
+        {
+            if (string.IsNullOrEmpty (this.Longitude.Text) ||
+               string.IsNullOrEmpty (this.Latitude.Text)) {
+                await DisplayAlert ("Invalid input", "Must enter both Longitude and Latitude", "Close");
+                return;
+            }
             var longitude = double.Parse (this.Longitude.Text);
             var latitude = double.Parse (this.Latitude.Text);
 
 
-            await _vm.GetWeatherAsync ( latitude, longitude);
+            await _vm.GetWeatherAsync (latitude, longitude);
 
+        }
+
+        public void OnShowAllClicked(Object o, EventArgs e)
+        {
+            
+        }
+
+
+        protected override void OnAppearing ()
+        {
+            _done = false;
+            Device.StartTimer (TimeSpan.FromSeconds (1), () => {
+                Resources ["CurrentTime"] = DateTime.Now.ToString ();
+                return (!_done);
+            });
+            base.OnAppearing ();
+        }
+
+        protected override void OnDisappearing ()
+        {
+            _done = true;
+            base.OnDisappearing ();
         }
     }
 }
---
+++ XFormWebService_SqlLite_Demo/XFormWebService_SqlLite_Demo/View/Trigger/ValidateInputInteger_Trigger.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFormWebService_SqlLite_Demo
+{
+
+
+    public class ValidateInputInteger_Trigger : TriggerAction<Entry>
+    {
+        protected override void Invoke (Entry sender)
+        {
+            int parsed;
+            bool valid = int.TryParse (sender.Text, out parsed);
+            if (!valid) {
+                sender.TextColor = Color.Red;
+            } else {
+                //sender.TextColor = Color.Black;
+            }
+        }
+    }
+
+}
+
